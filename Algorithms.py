7d0b07ecff79924f3abe795eb53bff7347a156a2
esc20936 <esc20936@uvg.edu.gt>
2022-09-14 21:47:18 -0600

    Thompson corregido

--- Algorithms.py
+++ Algorithms.py
@@ -4,54 +4,129 @@ class NFA:
         self.start = start
         self.end = end
 
+    def show(self):
+        self.start.show()
     
 class State:
-    def __init__(self, label=None, edges=None):
-        self.label = label
-        self.edges = edges or []
-        
-
-
-def Thompson(Regex):
-    nfaStack = []
-    for c in Regex:
-        if c == '?':
-            # Concatenation
-            nfa2 = nfaStack.pop()
-            nfa1 = nfaStack.pop()
-            # nfa1.end.edges.append(nfa2.start)
-            nfa1Edges = nfa1.end.edges
-            nfa2Edges = nfa2.start.edges
-            temp = State(edges=nfa1Edges + nfa2Edges)
-
-            nfa1.end = temp
-            nfa2.start = temp
-
-            # nfa1.end.label = nfa2.start.label
-            nfaStack.append(NFA(nfa1.start, nfa2.end))
-        elif c == '|':
-            # Alternation
-            nfa2 = nfaStack.pop()
-            nfa1 = nfaStack.pop()
-            start = State(edges=[nfa1.start, nfa2.start])
-            end = State()
-            nfa1.end.edges.append(end)
-            nfa2.end.edges.append(end)
-            nfaStack.append(NFA(start, end))
-        elif c == '*':
-            # Kleene star
-            nfa = nfaStack.pop()
-            start = State(edges=[nfa.start])
-            end = State()
-            nfa.end.edges += [nfa.start, end]
-            nfaStack.append(NFA(start, end))
-        else:
-            # Literal
-            end = State()
-            start = State(c, [end])
-            nfaStack.append(NFA(start, end))
+    def __init__(self, transitions=None):
+        self.transitions = transitions if transitions else {}
     
-    return nfaStack.pop()
+    def addTransition(self, symbol, state):
+        if symbol in self.transitions:
+            self.transitions[symbol].append(state)
+        else:
+            self.transitions[symbol] = [state]
+
+    # print evertything reachable from this state
+    def show(self, visited=None):
+        if visited is None:
+
+            visited = set()
+        if self in visited:
+            return
+        visited.add(self)
+        print(f"{self} =  {self.transitions}")
+        for key in self.transitions:
+            for state in self.transitions[key]:
+                state.show(visited)
+
+                
+# simbolo de epsilon = &
+def Thompson(Regex):
+    NFAstack = []
+    for char in Regex:
+        if char == '|':
+            nfa2 = NFAstack.pop()
+            nfa1 = NFAstack.pop()
+            start = State()
+            end = State()
+
+            start.addTransition('&', nfa1.start)
+            start.addTransition('&', nfa2.start)
+
+            nfa1.end.addTransition('&', end)
+            nfa2.end.addTransition('&', end)
+
+            NFAstack.append(NFA(start, end))
+        elif char == '?':
+            nfa2 = NFAstack.pop()
+            nfa1 = NFAstack.pop()
+            nfa1.end.transitions = {**nfa1.end.transitions,** nfa2.start.transitions}
+            NFAstack.append(NFA(nfa1.start, nfa2.end))
+
+        elif char == '*':
+            nfa = NFAstack.pop()
+            start = State()
+            end = State()
+
+            start.addTransition('&', nfa.start)
+            start.addTransition('&', end)
+
+            nfa.end.addTransition('&', nfa.start)
+            nfa.end.addTransition('&', end)
+
+            NFAstack.append(NFA(start, end))
+
+        elif char == '+':
+            nfa = NFAstack.pop()
+            start = State()
+            end = State()
+
+            start.addTransition('&', nfa.start)
+
+            nfa.end.addTransition('&', nfa.start)
+            nfa.end.addTransition('&', end)
+
+            NFAstack.append(NFA(start, end))
+
+
+        else:
+            end = State()
+            start = State(transitions={char: [end]})
+            NFAstack.append(NFA(start, end))
+            
+    return NFAstack.pop()
+
+# def Thompson(Regex):
+#     nfaStack = []
+#     for c in Regex:
+#         if c == '?':
+#             # Concatenation
+#             nfa2 = nfaStack.pop()
+#             nfa1 = nfaStack.pop()
+#             nfa1.end.edges.append(nfa2.start)
+#             # nfa1Edges = nfa1.end.edges
+#             # nfa2Edges = nfa2.start.edges
+#             # temp = State(edges=nfa1Edges + nfa2Edges)
+#             # nfa1.start.edges = [temp]
+#             # nfa1.end = temp
+#             # nfa2.start = temp
+
+#             # nfa1.end.label = nfa2.start.label
+#             nfaStack.append(NFA(nfa1.start, nfa2.end))
+#         elif c == '|':
+#             # Alternation
+#             nfa2 = nfaStack.pop()
+#             nfa1 = nfaStack.pop()
+#             start = State(edges=[nfa1.start, nfa2.start])
+#             end = State()
+#             nfa1.end.edges.append(end)
+#             nfa2.end.edges.append(end)
+#             nfaStack.append(NFA(start, end))
+#         elif c == '*':
+#             # Kleene star
+#             nfa = nfaStack.pop()
+#             start = State(edges=[nfa.start])
+#             end = State()
+#             nfa.end.edges += [nfa.start, end]
+#             nfaStack.append(NFA(start, end))
+#         else:
+#             # Literal
+#             end = State()
+#             start = State(c, [end])
+#             nfaStack.append(NFA(start, end))
+    
+#     return nfaStack.pop()
 
 # # parse Thomspon's NFA to DFA
 # def parseNFAtoDFA(nfa):
